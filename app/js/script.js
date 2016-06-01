e056aed56f6017ad13aadb969a3f1ed4af84eca8
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-06-01 14:19:21 -0400

    created collapsible nav bar

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+///// Small navbar slide animation
+  $(".nav-icon").click(function(){
+    $(".slide-down-nav").toggleClass("move-nav-down");
+    $(".small-logo").toggleClass("no-opacity");
+  });
+
+});
