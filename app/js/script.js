2943227c8f96db82d416f1666fa3995f080e7587
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-06-01 16:27:41 -0400

    added scroll effect to navigation bar

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
 
-///// Small navbar slide animation
+///// Small navbar drop down animation
   $(".nav-icon").click(function(){
     $(".slide-down-nav").toggleClass("move-nav-down");
-    $(".small-logo").toggleClass("no-opacity");
+    $(".small-logo").toggleClass("no-display");
   });
+
+
+//// navbar scroll to link effect
+
+  	$('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash;
+  	    var $target = $(target);
+
+        $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 900, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+      });
 
 });
