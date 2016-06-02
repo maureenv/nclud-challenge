29151a549917fca2956086b2312384c3e7ff56e4
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-06-01 21:03:08 -0400

    Added hover effect to buttons

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
 
-///// Small navbar drop down animation
+  ///// Small navbar drop down animation
   $(".nav-icon").click(function(){
+    $(".slide-down-nav").toggleClass("move-nav-down");
+    $(".small-logo").toggleClass("no-display");
+    });
+
+  $(".slide-down-li").click(function(){
     $(".slide-down-nav").toggleClass("move-nav-down");
     $(".small-logo").toggleClass("no-display");
   });
 
 
-//// navbar scroll to link effect
+  //// navbar scroll to link effect
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
 
-  	$('a[href^="#"]').on('click',function (e) {
-  	    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
 
-  	    var target = this.hash;
-  	    var $target = $(target);
-
-        $('html, body').stop().animate({
-  	        'scrollTop': $target.offset().top
-  	    }, 900, 'swing', function () {
-  	        window.location.hash = target;
-  	    });
-      });
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
 
 });
